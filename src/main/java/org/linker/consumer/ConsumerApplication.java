064719d272e7b9d5ff427703eabb6e5904d83f76
nguyen <rwmingad@163.com>
2018-11-06 17:21:41 +0800

    spring-cloud-eureka-consumer

---
+++ src/main/java/org/linker/consumer/ConsumerApplication.java
@@ -0,0 +1,24 @@
+package org.linker.consumer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ConsumerApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConsumerApplication.class).web(true).run(args);    }
+}
---
+++ src/main/java/org/linker/consumer/DcController.java
@@ -0,0 +1,21 @@
+package org.linker.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author RWM
+ * @date 2018/8/20
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    DcClient dcClient;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return dcClient.consumer();
+    }
+}
