fffbd05efd3f462665154c648a89c18fd89322f7
nguyen <rwmingad@163.com>
2018-11-28 00:29:38 +0800

    Spring Cloud Hystrix的请求合并

--- src/main/java/org/linker/consumer/controller/DcController.java
+++ src/main/java/org/linker/consumer/controller/DcController.java
@@ -1,9 +1,14 @@
 package org.linker.consumer.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.linker.consumer.feign.DcClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author RWM
@@ -15,8 +20,26 @@ public class DcController {
     @Autowired
     private DcClient dcClient;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/consumer")
     public String consumer() {
         return dcClient.dc();
     }
+
+    @GetMapping("/dc")
+    public String dc() {
+        return restTemplate.getForObject("http://eureka-client/dc/{1}", String.class, 0);
+    }
+
+    @GetMapping("/dcs")
+    public List dcs() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        ids.add(2L);
+        ids.add(3L);
+        return restTemplate.getForObject("http://eureka-client/dc?ids={1}", List.class, StringUtils.join(ids, ","));
+    }
+
 }
